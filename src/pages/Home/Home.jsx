5e9c8e5a8d4a36073a89b994c3db388f5f441ddb
Ryan Yosua <ryan.yosua@nutrien.com>
2020-02-15 09:58:22 -0700

    home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-const Home = () => <p>Take a user test. Earn bux.</p>;
+const Home = () => (
+  <>
+    <Typography variant="h1">Take a user test. Earn bux.</Typography>
+    <Box display="flex" flex={1}>
+      <Box>
+        <Typography>for everyone</Typography>
+        <Button>Take a test</Button>
+      </Box>
+      <Box>
+        <Typography>for devs</Typography>
+        <Button> Create a test</Button>
+      </Box>
+    </Box>
+  </>
+);
 
 export default Home;
