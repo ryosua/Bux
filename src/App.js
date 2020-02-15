f00f9cccc7cdea04f562dd52bbc3a5d0760771a4
Ryan Yosua <ryan.yosua@nutrien.com>
2020-02-15 09:43:08 -0700

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import Home from "./pages/Home";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Take a user test. Earn bux.</p>
+        <Home />
       </header>
     </div>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Home = () => <p>Take a user test. Earn bux.</p>;
+
+export default Home;
