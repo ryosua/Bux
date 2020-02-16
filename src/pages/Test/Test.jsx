e279d04b4f743c327f08d343bdc9b570ba3f3996
Ryan Yosua <ryan.yosua@nutrien.com>
2020-02-16 10:25:17 -0700

    test screen

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 
 export default function() {
-  return <div></div>;
+  return (
+    <>
+      <Typography variant="h2">Preference Test</Typography>
+      <Typography>Which option do you prefer?</Typography>
+      <TextField />
+      <Typography>Why did you select that option?</Typography>
+      <TextField />
+      <Button>Submit</Button>
+    </>
+  );
 }
